16e306a3828e4a2317fc5362ceecff68260fdb15
David Dao <daothehoang@gmail.com>
2021-04-06 15:46:56 +0700

    +

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,12 @@
 use App\Http\Controllers\SellingManagerController;
 use App\Http\Middleware\Authenticate;
 
+Route::get('/', function () {           return view('welcome'); });
+
 Route::group(['middleware' => ['get.menu']], function () {    
 
-    Route::get('/', function () {           return view('welcome'); });
     Route::group(['middleware' => ['role:user']], function () {
-        Route::get('/', function () {           return view('dashboard.homepage'); });
+        Route::get('/dashboard', function () {           return view('dashboard.homepage'); });
         Route::get('/colors', function () {     return view('dashboard.colors'); });
         Route::get('/typography', function () { return view('dashboard.typography'); });
         Route::get('/charts', function () {     return view('dashboard.charts'); });
