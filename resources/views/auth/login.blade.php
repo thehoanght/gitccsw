e20af4c44a6b612ac65030203d68e26b89da801f
David Dao <daothehoang@gmail.com>
2021-04-06 15:55:06 +0700

    +

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,7 +49,8 @@
                   <h2>Sign up</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                   @if (Route::has('password.request'))
-                    <a href="{{ route('register') }}" class="btn btn-primary active mt-3">{{ __('Register') }}</a>
+                    <!-- <a href="{{ route('register') }}" class="btn btn-primary active mt-3">{{ __('Register') }}</a> -->
+                    <a href="#" class="btn btn-primary active mt-3">{{ __('Register') }}</a>
                   @endif
                 </div>
               </div>
