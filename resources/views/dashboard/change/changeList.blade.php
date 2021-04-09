c2243decececf89dcb4c10c0209c6d888648ca40
David Dao <daothehoang@gmail.com>
2021-04-09 23:21:58 +0700

    +

--- resources/views/dashboard/change/changeList.blade.php
+++ resources/views/dashboard/change/changeList.blade.php
@@ -96,7 +96,7 @@
                                                 @endif
                                             </td>
                                             <td><a href="{{ url('/crawl/' . $data->email_old_id ) }}">{{ strtolower(DB::table('etsy_accounts')->find($data->email_old_id)->email_old)}}</a></td>
-                                            <td>{{ DB::table('email_accounts')->find($data->email_new_id)->email  }}</td>
+                                            <td>{{ DB::table('email_accounts')->find($data->email_new_id)->email }}:{{ DB::table('email_accounts')->find($data->email_new_id)->password  }}</td>
                                             <td class="text-center">
                                                 @if (DB::table('etsy_accounts')->find($data->email_old_id)->purchased != 'TRUE')
                                                     <span class="badge badge-secondary">FALSE</span>
